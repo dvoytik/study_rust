0ae11612ae50f781eb8a5cbd600ed592faf80f4a
Dmitry Voytik <voytikd@gmail.com>
2024-07-29 22:20:00 +0200

    arrays and slices
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,6 +1,8 @@
 use std::mem;
 
-fn type_of<T>(_: &T) -> &str {
+// I had to make T: ?Sized to allow unsized types.
+// Note that unsized typed can be accessed only via a reference &T
+fn type_of<T: ?Sized>(_: &T) -> &str {
     std::any::type_name::<T>()
 }
 
@@ -16,6 +18,7 @@ fn main() {
 
     let array = [3, 5, 7, 11]; // type is [i32; 4]
     println!("array: {array:?}, its size is {}", mem::size_of_val(&array));
+    assert_eq!(mem::size_of_val(&array), 4 * 4);
     // array: [3, 5, 7, 11]
     println!("sizeof(array) = {}", std::mem::size_of::<[i32; 4]>());
     // sizeof(array) = 16
@@ -36,16 +39,21 @@ fn main() {
 
     // WARNING: This is not a slice but an array of one element of type RangeFrom<i32>
     let _slice = [0..];
+    println!("typeof([0..]): {}", type_of(&_slice));
     let slice = &[0..]; // same as &_slice
     println!("slice: {:?}", slice);
 
     // TODO: RangeFrom
     // assert_eq!((2..), std::ops::RangeFrom { start: 2 });
     let arr = [0, 1, 2, 3, 4];
+    assert_eq!(type_of(&arr), "[i32; 5]");
+    assert_eq!(type_of(&arr[..]), "[i32]");
     assert_eq!(arr[..], [0, 1, 2, 3, 4]);
     assert_eq!(arr[..3], [0, 1, 2]);
+    assert_eq!(type_of(&arr[..3]), "[i32]");
     assert_eq!(arr[..=3], [0, 1, 2, 3]);
-    assert_eq!(arr[1..], [1, 2, 3, 4]); // This is a `RangeFrom`
+    assert_eq!(arr[1..], [1, 2, 3, 4]);
+    assert_eq!(type_of(&arr[1..]), "[i32]");
     assert_eq!(arr[1..3], [1, 2]);
     assert_eq!(arr[1..=3], [1, 2, 3]);
 }
